f4582f37cec3c084272c9b50d81a5dbdf1e03479
dahuya <87560662+dahuya@users.noreply.github.com>
2025-12-24 16:35:28 +0800

    Update jest.env.js

--- jest.env.js
+++ jest.env.js
@@ -3,9 +3,9 @@
 
 // Set test environment variables
 process.env.NODE_ENV = 'test'
-process.env.NEXT_PUBLIC_TITLE = 'Test Blog'
-process.env.NEXT_PUBLIC_DESCRIPTION = 'Test Description'
-process.env.NEXT_PUBLIC_AUTHOR = 'Test Author'
+process.env.NEXT_PUBLIC_TITLE = '陈的博客'
+process.env.NEXT_PUBLIC_DESCRIPTION = '一个中年老光棍'
+process.env.NEXT_PUBLIC_AUTHOR = 'chen'
 process.env.NEXT_PUBLIC_LANG = 'zh-CN'
 process.env.NEXT_PUBLIC_THEME = 'test'
 process.env.NEXT_PUBLIC_LINK = 'https://test.com'
